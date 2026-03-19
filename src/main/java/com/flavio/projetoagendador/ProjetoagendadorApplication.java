b16d72d37c8a31bbe094c1bf7ddc3d0323d048b5
Flavio <flaviongo21@gmail.com>
2026-03-18 23:02:12 -0300

    commit inicial

---
+++ src/main/java/com/flavio/projetoagendador/ProjetoagendadorApplication.java
@@ -0,0 +1,13 @@
+package com.flavio.projetoagendador;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoagendadorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoagendadorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/flavio/projetoagendador/ProjetoagendadorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flavio.projetoagendador;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoagendadorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
